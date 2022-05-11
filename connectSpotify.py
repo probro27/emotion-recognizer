09d52e3dc0007dc6b8528d266b8afaa6c9fa0666
probro27 <prabhavkhera@hotmail.com>
2022-05-11 16:26:21 -0400

    Requirements added

--- connectSpotify.py
+++ connectSpotify.py
@@ -15,12 +15,6 @@ client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
 redirect_uri = "https://prabhav-khera.netlify.com"
 
 def connect(username):
-    # client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-    # sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-    # url = "	https://api.spotify.com/v1/me"
-    # response = requests.get(url, headers={"Authorization": "Bearer " + os.getenv("SPOTIFY_TOKEN")})
-    # # username = requests.get(url).json()['display_name']
-    # username = response.json()['display_name']
     scope = "user-library-read user-top-read playlist-modify-public user-follow-read"
     token = util.prompt_for_user_token(username, scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
     os.environ['SPOTIFY_TOKEN'] = token
